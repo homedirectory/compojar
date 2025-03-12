a990ee7d8d9251519ef35e42195864e84522dd23
homedirectory <nullsink@posteo.eu>
2025-03-12 19:32:15 +0200

    Fluent API for BNF definitions

--- compojar-core/src/main/java/compojar/bnf/BnfBuilder.java
+++ compojar-core/src/main/java/compojar/bnf/BnfBuilder.java
@@ -3,22 +3,37 @@ package compojar.bnf;
 import java.util.ArrayList;
 import java.util.List;
 
-// public final class BnfBuilder {
-//
-//     private final Variable start;
-//     private final List<Rule> rules;
-//
-//     BnfBuilder(final Variable start) {
-//         this.start = start;
-//         rules = new ArrayList<>();
-//     }
-//
-//     public BnfBuilder addRule(Rule rule) {
-//         this.rules.add(rule);
-//     }
-//
-//     public BNF build() {
-//         return new BNF(rules, start);
-//     }
-//
-// }
+//  start(S)
+// .select(S, X, Y)
+// .derive(X, x)
+// .$()
+
+public final class BnfBuilder {
+
+    public static BnfBuilder start(Variable v) {
+        return new BnfBuilder(v);
+    }
+
+    private final Variable start;
+    private final List<Rule> rules;
+
+    BnfBuilder(final Variable start) {
+        this.start = start;
+        rules = new ArrayList<>();
+    }
+
+    public BnfBuilder select(Variable lhs, Variable rhsHd, Variable... rhsRest) {
+        rules.add(Rule.selection(lhs, rhsHd, rhsRest));
+        return this;
+    }
+
+    public BnfBuilder derive(Variable lhs, Symbol... rhs) {
+        rules.add(Rule.derivation(lhs, rhs));
+        return this;
+    }
+
+    public BNF $() {
+        return new BNF(rules, start);
+    }
+
+}
